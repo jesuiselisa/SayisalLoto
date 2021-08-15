c1be671cc264e8eb65fea73b153f409c7c7860c0
BADEM <elisaucmak@gmail.com>
2021-08-15 21:12:27 +0300

    Add project files.

---
+++ SayisalLoto/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace SayisalLoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnBaslat = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btnSifirla = new Bunifu.Framework.UI.BunifuThinButton2();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(128, 239);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(59, 50);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "*";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SayisalLoto.Properties.Resources._5f6d0fb9c8c3730d0867bd6e;
+            this.pictureBox1.Location = new System.Drawing.Point(241, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl2
+            // 
+            this.lbl2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(221, 239);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(59, 50);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "*";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl3.Font = new System.Drawing.Font("Segoe UI", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl3.Location = new System.Drawing.Point(310, 239);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(59, 50);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "*";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl4
+            // 
+            this.lbl4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl4.Font = new System.Drawing.Font("Segoe UI", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl4.Location = new System.Drawing.Point(398, 239);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(59, 50);
+            this.lbl4.TabIndex = 4;
+            this.lbl4.Text = "*";
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl5
+            // 
+            this.lbl5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl5.Font = new System.Drawing.Font("Segoe UI", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl5.Location = new System.Drawing.Point(486, 239);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(59, 50);
+            this.lbl5.TabIndex = 5;
+            this.lbl5.Text = "*";
+            this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl6
+            // 
+            this.lbl6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl6.Font = new System.Drawing.Font("Segoe UI", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl6.Location = new System.Drawing.Point(572, 239);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(59, 50);
+            this.lbl6.TabIndex = 6;
+            this.lbl6.Text = "*";
+            this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Red;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(243, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(283, 39);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "KAZANAN RAKAMLAR:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.ActiveBorderThickness = 1;
+            this.btnBaslat.ActiveCornerRadius = 20;
+            this.btnBaslat.ActiveFillColor = System.Drawing.Color.DodgerBlue;
+            this.btnBaslat.ActiveForecolor = System.Drawing.Color.White;
+            this.btnBaslat.ActiveLineColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBaslat.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnBaslat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBaslat.BackgroundImage")));
+            this.btnBaslat.ButtonText = "ÇEKİLİŞİ BAŞLAT";
+            this.btnBaslat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBaslat.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaslat.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBaslat.IdleBorderThickness = 1;
+            this.btnBaslat.IdleCornerRadius = 20;
+            this.btnBaslat.IdleFillColor = System.Drawing.Color.Red;
+            this.btnBaslat.IdleForecolor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBaslat.IdleLineColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBaslat.Location = new System.Drawing.Point(241, 320);
+            this.btnBaslat.Margin = new System.Windows.Forms.Padding(5);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(135, 66);
+            this.btnBaslat.TabIndex = 8;
+            this.btnBaslat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.ActiveBorderThickness = 1;
+            this.btnSifirla.ActiveCornerRadius = 20;
+            this.btnSifirla.ActiveFillColor = System.Drawing.Color.DodgerBlue;
+            this.btnSifirla.ActiveForecolor = System.Drawing.Color.White;
+            this.btnSifirla.ActiveLineColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSifirla.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSifirla.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSifirla.BackgroundImage")));
+            this.btnSifirla.ButtonText = "SIFIRLA";
+            this.btnSifirla.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSifirla.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSifirla.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSifirla.IdleBorderThickness = 1;
+            this.btnSifirla.IdleCornerRadius = 20;
+            this.btnSifirla.IdleFillColor = System.Drawing.Color.Red;
+            this.btnSifirla.IdleForecolor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSifirla.IdleLineColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSifirla.Location = new System.Drawing.Point(391, 320);
+            this.btnSifirla.Margin = new System.Windows.Forms.Padding(5);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(135, 66);
+            this.btnSifirla.TabIndex = 9;
+            this.btnSifirla.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnSifirla.Click += new System.EventHandler(this.btnSifirla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(769, 434);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sayısal Loto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label label7;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnBaslat;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnSifirla;
+    }
+}
+
---
+++ SayisalLoto/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayisalLoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            lbl1.Text = rnd.Next(1, 49).ToString();
+            lbl2.Text = rnd.Next(1, 49).ToString();
+            lbl3.Text = rnd.Next(1, 49).ToString();
+            lbl4.Text = rnd.Next(1, 49).ToString();
+            lbl5.Text = rnd.Next(1, 49).ToString();
+            lbl6.Text = rnd.Next(1, 49).ToString();
+        }
+
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = "*";
+            lbl2.Text = "*";
+            lbl3.Text = "*";
+            lbl4.Text = "*";
+            lbl5.Text = "*";
+            lbl6.Text = "*";
+        }
+    }
+}
